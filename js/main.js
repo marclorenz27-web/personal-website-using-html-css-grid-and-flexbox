65508730f86c580fe98a9aad9c3109fef6cfc007
Marc Lorenz Comia <comia.marclorenz@gmail.com>
2021-04-07 21:32:49 +0800

    Updated animations, grid/flex and mobile layout

---
+++ js/main.js
@@ -0,0 +1,12 @@
+{/* <script>
+    function toggleMenu() {
+        let mobileMenu = document.querySelector('#mobile-nav');
+    if (mobileMenu.classList == '') {
+        mobileMenu.classList.add('show');
+    }
+    else {
+        mobileMenu.classList.remove('show');
+    }
+    console.log('Button Clicked');
+}
+</script> */}
